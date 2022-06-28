6a1a1527f81b2fb54fee124575e98957afb33592
Ahmad Rifai <87814697+Ahmdrf27@users.noreply.github.com>
2022-06-28 12:59:40 +0700

    Add files via upload

---
+++ SourceCode/aplikasiVisualisasiData/src/aplikasivisualisasidata/AplikasiVisualisasiData.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasivisualisasidata;
+
+/**
+ *
+ * @author HP
+ */
+public class AplikasiVisualisasiData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        log lg = new log();
+        lg.setVisible(true);
+        lg.pack();
+        lg.setLocationRelativeTo(null);
+        lg.setDefaultCloseOperation(log.EXIT_ON_CLOSE);
+    }
+    
+}
